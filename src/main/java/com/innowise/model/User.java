11b4f7a8c501e77bb61317cc0866ecb243ff1ead
Nikita <nikitakarpinghikus@gmail.com>
2025-09-17 00:19:33 +0300

    feat(entities): implement full CRUD for users and cards

---
+++ src/main/java/com/innowise/model/User.java
@@ -0,0 +1,52 @@
+package com.innowise.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"cards"})
+@EqualsAndHashCode
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Card> cards;
+}
---
+++ src/main/java/com/innowise/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.innowise.repository;
+
+import com.innowise.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByIdIn(List<Long> ids);
+
+    @Query("SELECT u FROM User u WHERE u.email = :userEmail")
+    Optional<User> findByEmail(@Param("userEmail") String email);
+
+    @Modifying
+    @Query(value = "UPDATE users SET name = COALESCE(:name, name), surname = COALESCE(:surname, surname), email = COALESCE(:email, email) WHERE id = :id", nativeQuery = true)
+    int updateUser(@Param("id") Long id, @Param("name") String name, @Param("surname") String surname, @Param("email") String email);
+}
