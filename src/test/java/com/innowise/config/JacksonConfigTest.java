432c522f7702947e716d744b30d03cdbfff4da0b
Nikita <nikitakarpinghikus@gmail.com>
2025-09-27 00:57:02 +0300

    fix(sonar-check): add test for JacksonConfig

---
+++ src/test/java/com/innowise/config/JacksonConfigTest.java
@@ -0,0 +1,24 @@
+package com.innowise.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = JacksonConfig.class)
+class JacksonConfigTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void objectMapperBeanShouldExist() {
+        assertAll(
+                () -> assertNotNull(objectMapper),
+                () -> assertFalse(objectMapper.isEnabled(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS))
+        );
+    }
+}
