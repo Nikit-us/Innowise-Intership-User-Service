eebb70812a74720568c573da7236ecfdd5efd9b6
Nikita <nikitakarpinghikus@gmail.com>
2025-10-14 18:30:16 +0300

    refactor: move extract token to a separate class

--- src/main/java/com/innowise/exception/CustomSecurityHandlers.java
+++ src/main/java/com/innowise/exception/CustomSecurityHandlers.java
@@ -3,6 +3,7 @@ package com.innowise.exception;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
@@ -13,9 +14,9 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class CustomSecurityHandlers implements AuthenticationEntryPoint, AccessDeniedHandler {
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
--- src/main/java/com/innowise/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/innowise/filter/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.innowise.filter;
 import com.innowise.dto.ValidateTokenRequest;
 import com.innowise.dto.ValidateTokenResponse;
 import com.innowise.service.AuthenticationServiceClient;
+import com.innowise.util.JwtTokenUtils;
 import feign.FeignException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -19,29 +20,28 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final AuthenticationServiceClient authenticationServiceClient;
+    private final JwtTokenUtils jwtTokenUtils;
 
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
-        String header = request.getHeader("Authorization");
-        log.info("Authorization header: {}", header);
-        if (header == null || !header.startsWith("Bearer ")) {
+        Optional<String> token = jwtTokenUtils.extractToken(request);
+
+        if (token.isEmpty()) {
             filterChain.doFilter(request, response);
             return;
         }
-        String token = header.substring(7);
-        log.info("Authentication Token: {}", token);
         try {
-            ValidateTokenResponse validation = authenticationServiceClient.validate(new ValidateTokenRequest(token));
-            log.info("Validate Token: {}", validation);
+            ValidateTokenResponse validation = authenticationServiceClient.validate(new ValidateTokenRequest(token.get()));
             if(validation.valid() && validation.tokenType().equals("access_token")) {
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                         validation.userId(),
